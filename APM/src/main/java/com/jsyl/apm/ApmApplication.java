bd080faacafe525238987b983932dcac94aef48c
cnt-system <cnt@example.com>
2026-02-27 10:35:32 +0800

    第一次提交

---
+++ APM/src/main/java/com/jsyl/apm/ApmApplication.java
@@ -0,0 +1,13 @@
+package com.jsyl.apm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApmApplication.class, args);
+    }
+
+}
